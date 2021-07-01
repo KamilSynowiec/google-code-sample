7239b6d0869c722d65dab2d8e6194bccfe01f838
Kamil Synowiec <1935002@brunel.ac.uk>
2021-07-01 07:30:08 +0100

    Update VideoPlaylist.java

--- java/src/main/java/com/google/VideoPlaylist.java
+++ java/src/main/java/com/google/VideoPlaylist.java
@@ -1,6 +1,17 @@
 package com.google;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /** A class used to represent a Playlist */
 class VideoPlaylist {
+
+    List<Video> videos = new <Video>ArrayList();
+
+    void add(Video video)
+    {
+        videos.add(video);
+    }
 
 }
